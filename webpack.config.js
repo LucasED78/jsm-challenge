6821ea57dbbc3a3a0b44dffd02e873e7042c87e1
Lucas Eduardo <lucased78@hotmail.com>
2020-03-20 08:44:01 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test:/\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|ttf)$/i,
+        use: 'url-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': 'src' 
+    }
+  }
+}
